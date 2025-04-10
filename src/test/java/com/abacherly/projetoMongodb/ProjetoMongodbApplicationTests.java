c04db1bd585102e244fb68e5eef70359741a1bf5
Abacherly <mariana.abacherly@gmail.com>
2025-04-09 22:32:20 -0300

    Conectando ao MongoDB com repository e service

--- src/test/java/com/abacherly/projetoMongodb/ProjetoMongodbApplicationTests.java
+++ src/test/java/com/abacherly/projetoMongodb/ProjetoMongodbApplicationTests.java
@@ -2,12 +2,12 @@ package com.abacherly.projetoMongodb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-class ProjetoMongodbApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ProjetoMongodbApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
